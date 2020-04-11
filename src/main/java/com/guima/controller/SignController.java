e0ff5203593ef33b30b4272fd79c2820946b29c2
然 <18911158295@163.com>
2020-04-11 23:54:04 +0800

    feature_test1

--- src/main/java/com/guima/controller/SignController.java
+++ src/main/java/com/guima/controller/SignController.java
@@ -35,6 +35,7 @@ public class SignController extends BaseController{
      * 创建打卡
      */
     public void savePlanSign(){
+        //feature_test1
         User user=getMyUser();
         checkUser(user);
         Sign sign=new Sign();
